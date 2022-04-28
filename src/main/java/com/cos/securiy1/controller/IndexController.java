1d7f0c4af7e3f5f0b2b55ba55ffdcfca7711da7f
Donggyu Han <lucky5632@naver.com>
2022-04-28 12:11:29 +0900

    Controller 테스트 함수 추가

--- src/main/java/com/cos/securiy1/controller/IndexController.java
+++ src/main/java/com/cos/securiy1/controller/IndexController.java
@@ -2,6 +2,7 @@ package com.cos.securiy1.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller // View Return
 public class IndexController {
@@ -11,5 +12,36 @@ public class IndexController {
         // 머스테치 기본폴더 src/main/resources/
         // ViewResolver 설정 : templates (prefix) , mustache(suffix) - 생략 가능.
         return "index"; // src/main/resources/templates/index.mustache 를 찾게된다.
+    }
+
+    @GetMapping("/user")
+    public String user(){
+        return "user";
+    }
+
+    @GetMapping("/admin")
+    public String admin(){
+        return "admin";
+    }
+
+    @GetMapping("/manager")
+    public String manager(){
+        return "manager";
+    }
+
+    // 스프링 시큐리티가 해당 주소를 낚아챈다.
+    @GetMapping("/login")
+    public String login(){
+        return "login";
+    }
+
+    @GetMapping("/join")
+    public String join(){
+        return "join";
+    }
+
+    @GetMapping("/joinProc")
+    public @ResponseBody String joinProc(){
+        return "회원가입 완료됨!";
     }
 }
