9d8c3d3fb985b1baeea805d6e1bd91d38487a4e0
Donggyu Han <lucky5632@naver.com>
2022-05-01 20:56:46 +0900

    User Entity / Repository 구현

---
+++ src/main/java/com/cos/securiy1/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.cos.securiy1.repository;
+
+import com.cos.securiy1.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+// CRUD 함수를 들고있음
+// @Repository 어노테이션이 없어도 IOC가 된다.
+public interface UserRepository extends JpaRepository<User,Integer> {
+
+}
