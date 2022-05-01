a503d187e45351b560ccd8cd4007c8b2b8b7b290
Donggyu Han <lucky5632@naver.com>
2022-05-01 21:30:21 +0900

    Security Session, Authentication, UserDetails

---
+++ src/main/java/com/cos/securiy1/auth/PrincipalDetailsService.java
@@ -0,0 +1,28 @@
+package com.cos.securiy1.auth;
+
+import com.cos.securiy1.model.User;
+import com.cos.securiy1.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+// Security 설정에서 loginProcessUrl("/login")으로 걸어뒀기 때문에 /login 요청이오면 자동으로 타입이 IoC 되어있는
+// loadUserByUsername 함수가 수행 => "약속"
+@Service
+public class PrincipalDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    // 시큐리티 session(내부 Authentication(내부 UserDetails))
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User userEntity = userRepository.findByUsername(username);
+        if(userEntity != null){
+            return new PrincipalDetails(userEntity);
+        }
+        return null;
+    }
+}
--- src/main/java/com/cos/securiy1/repository/UserRepository.java
+++ src/main/java/com/cos/securiy1/repository/UserRepository.java
@@ -6,5 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 // CRUD 함수를 들고있음
 // @Repository 어노테이션이 없어도 IOC가 된다.
 public interface UserRepository extends JpaRepository<User,Integer> {
-
+    // findBy규칙 -> Username 문법
+    // select * from user where username = ?
+    public User findByUsername(String username);
 }
