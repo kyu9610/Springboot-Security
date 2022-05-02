738a54cf2a5942a7c7e7e3c891e35edb66c76720
Donggyu Han <lucky5632@naver.com>
2022-05-02 18:16:06 +0900

    OAuth2 Google Login 기본 설정

--- src/main/java/com/cos/securiy1/config/SecurityConfig.java
+++ src/main/java/com/cos/securiy1/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.cos.securiy1.config;
 
+import com.cos.securiy1.oauth.PrincipalOauth2UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -12,6 +14,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @EnableWebSecurity // 활성화 , 스프링 시큐리티 필터가 스프링 필터 체인에 등록
 @EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true) // secured 어노테이션 활성화, preAuthorize/postAuthorize 어노테이션 활성화
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private PrincipalOauth2UserService principalOauth2UserService;
 
     // 해당 메서드의 리턴되는 오브젝트를 IoC로 등록해준다.
     @Bean
@@ -31,6 +36,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .formLogin()
                 .loginPage("/loginForm") // 로그인 페이지 설정
                 .loginProcessingUrl("/login") // login 주소가 호출되면 시큐리티가 로그인을 진행해 준다.
-                .defaultSuccessUrl("/"); // 완료시 메인 페이지로 이동
+                .defaultSuccessUrl("/") // 완료시 메인 페이지로 이동
+                .and()
+                .oauth2Login()
+                .loginPage("/loginForm") // 구글 로그인 완료까지, 이후 후처리가 필요
+                // 1. 코드받기(인증) 2. access Token(권한), 3.사용자 프로필 정보를 가져오기 4.그 정보를 토대로 회원가입 자동진행
+                // if) 추가적인 고객정보가 필요하게되면 추가적인 회원가입 view가 필요하다.
+                // 구글 로그인시 -> access Token + 사용자 프로필을 한번에 받음.
+                .userInfoEndpoint()
+                .userService(principalOauth2UserService);
     }
 }
