e70d11b6998dc0436ad62bdd219c7f0e3315a734
Donggyu Han <lucky5632@naver.com>
2022-05-02 15:19:13 +0900

    @EnableGlobalMethodSecurity 어노테이션을 통해서 시큐리티 권한 처리 테스트

--- src/main/java/com/cos/securiy1/controller/IndexController.java
+++ src/main/java/com/cos/securiy1/controller/IndexController.java
@@ -3,6 +3,8 @@ package com.cos.securiy1.controller;
 import com.cos.securiy1.model.User;
 import com.cos.securiy1.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -26,17 +28,17 @@ public class IndexController {
     }
 
     @GetMapping("/user")
-    public String user(){
+    public @ResponseBody String user(){
         return "user";
     }
 
     @GetMapping("/admin")
-    public String admin(){
+    public @ResponseBody String admin(){
         return "admin";
     }
 
     @GetMapping("/manager")
-    public String manager(){
+    public @ResponseBody String manager(){
         return "manager";
     }
 
@@ -64,5 +66,18 @@ public class IndexController {
     @GetMapping("/joinProc")
     public @ResponseBody String joinProc(){
         return "redirect:/loginForm";
+    }
+
+    @Secured("ROLE_ADMIN") // 간단하게 걸고싶을때 (하나만 걸고싶을때)
+    @GetMapping("/info")
+    public @ResponseBody String info(){
+        return "개인정보";
+    }
+
+    //@PostAuthorzie , 메소드 실행 후에
+    @PreAuthorize("hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')") // data 메소드 실행 직전에
+    @GetMapping("/data")
+    public @ResponseBody String data(){
+        return "데이터정보";
     }
 }
