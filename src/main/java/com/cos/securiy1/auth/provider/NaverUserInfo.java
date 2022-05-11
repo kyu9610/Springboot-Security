8f2411c93d7ad338afac79d81d0e53f66955ae55
Donggyu Han <lucky5632@naver.com>
2022-05-11 18:56:00 +0900

    네이버 로그인을 구분하기 위한 클래스 추가

---
+++ src/main/java/com/cos/securiy1/auth/provider/NaverUserInfo.java
@@ -0,0 +1,32 @@
+package com.cos.securiy1.auth.provider;
+
+import java.util.Map;
+
+public class NaverUserInfo implements OAuth2UserInfo{
+    private Map<String,Object> attributes; // getAttributes;
+
+    // 생성자
+    public NaverUserInfo(Map<String,Object> attributes){
+        this.attributes = attributes;
+    }
+
+    @Override
+    public String getProviderId() {
+        return (String)attributes.get("id");
+    }
+
+    @Override
+    public String getProvider() {
+        return "naver";
+    }
+
+    @Override
+    public String getEmail() {
+        return (String)attributes.get("email");
+    }
+
+    @Override
+    public String getName() {
+        return (String)attributes.get("name");
+    }
+}
